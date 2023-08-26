c832d60f40cf24ac7d4e1926446904fead691161
hyeseung <wjoo3@naver.com>
2023-08-27 00:47:46 +0900

    [UPDATE] firebase 설치

---
+++ firebase.js
@@ -0,0 +1,19 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyDacJXD7C4PF8bCSy8ZCfiE73vToAp0LxA",
+  authDomain: "react-poke-app-4e778.firebaseapp.com",
+  projectId: "react-poke-app-4e778",
+  storageBucket: "react-poke-app-4e778.appspot.com",
+  messagingSenderId: "31293600534",
+  appId: "1:31293600534:web:4ac1ac34548f9d4f34d637",
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+
+export default app;
