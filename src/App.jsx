3d580f35d797cd6dbd305b8d6049574c9a4c37a3
hyeseung <wjoo3@naver.com>
2023-08-27 01:04:28 +0900

    [UPDATE] Layout 생성

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,32 @@
-import { BrowserRouter, Route, Routes } from "react-router-dom";
+import { BrowserRouter, Outlet, Route, Routes } from "react-router-dom";
 import MainPage from "./pages/MainPage";
 import DetailPage from "./pages/DetailPage";
 import LoginPage from "./pages/LoginPage";
+import NavBar from "./components/NavBar";
+
+// 중첩을 위한 Layout
+const Layout = () => {
+  return (
+    <>
+      <NavBar />
+      <br />
+      <br />
+      <br />
+      {/* Outlet 부분에 Layout Route 안에 중첩된 Route 들이 온다 */}
+      <Outlet />
+    </>
+  );
+};
 
 function App() {
   return (
     <BrowserRouter>
       <Routes>
-        <Route path="/" element={<MainPage />}></Route>
-        <Route path="login" element={<LoginPage />}></Route>
-        <Route path="/pokemon/:id" element={<DetailPage />}></Route>
+        <Route path="/" element={<Layout />}>
+          <Route index element={<MainPage />} />
+          <Route path="login" element={<LoginPage />} />
+          <Route path="/pokemon/:id" element={<DetailPage />} />
+        </Route>
       </Routes>
     </BrowserRouter>
   );
