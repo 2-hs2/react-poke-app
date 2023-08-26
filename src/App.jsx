d03c93d42bf94b7ba039623c28b9f21394efa126
hyeseung <wjoo3@naver.com>
2023-08-27 00:49:28 +0900

    [UPDATE] LoginPage Route 추가

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,14 @@
-import { BrowserRouter, Route, Routes } from 'react-router-dom';
-import MainPage from './pages/MainPage';
-import DetailPage from './pages/DetailPage';
+import { BrowserRouter, Route, Routes } from "react-router-dom";
+import MainPage from "./pages/MainPage";
+import DetailPage from "./pages/DetailPage";
+import LoginPage from "./pages/LoginPage";
 
 function App() {
   return (
     <BrowserRouter>
       <Routes>
         <Route path="/" element={<MainPage />}></Route>
+        <Route path="login" element={<LoginPage />}></Route>
         <Route path="/pokemon/:id" element={<DetailPage />}></Route>
       </Routes>
     </BrowserRouter>
